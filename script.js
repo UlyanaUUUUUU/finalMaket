46d2fa6a9b44b0a737d63ee641ea21d35ec482dd
Ульяна <panfilovaulyana2016@mail.ru>
2024-10-12 00:42:34 +0500

    Внесла правки

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function swiperCard() {
             swiper = new Swiper(".mySwiper", {
                 direction: "horizontal",
                 slidesPerView: "auto",
-                centeredSlides: true,
                 spaceBetween: 16,
                 pagination: {
                     el: ".swiper-pagination",
@@ -26,45 +25,62 @@ swiperCard();
 window.addEventListener("resize", swiperCard);
 
 
-let slide = document.querySelectorAll('.swiper-slide');
-let showMore = document.querySelector('.show');
-let hide = document.querySelector('.hide');
+let slide = document.querySelectorAll('.brands__item');
+let button = document.querySelector('.readMore');
+let showMoreIcon = document.querySelector('.readMore__icon');
+let showMoreText = document.querySelector('.readMore__text');
 
-if (window.innerWidth >= 768 && window.innerWidth < 1120) {
+
+if (window.innerWidth >= 768 && window.innerWidth < 1067) {
     for (let i = 0; i < slide.length; i++) {
         if (i > 5) {
-            slide[i].classList.add('hidden');
-            hide.classList.add('hidden');
-            showMore.addEventListener('click', function () {
-                slide[i].classList.remove('hidden');
-                showMore.classList.add('hidden');
-                hide.classList.remove('hidden');
-            });
-            hide.addEventListener('click', function () {
-                slide[i].classList.add('hidden');
-                hide.classList.add('hidden');
-                showMore.classList.remove('hidden');
-            });
+            slide[i].classList.add('hidden')
         }
     }
-
 }
-
-if (window.innerWidth >= 1120) {
+if (window.innerWidth >= 1067) {
     for (let i = 0; i < slide.length; i++) {
         if (i > 7) {
-            slide[i].classList.add('hidden');
-            hide.classList.add('hidden');
-            showMore.addEventListener('click', function () {
-                slide[i].classList.remove('hidden');
-                showMore.classList.add('hidden');
-                hide.classList.remove('hidden');
-            });
-            hide.addEventListener('click', function () {
-                slide[i].classList.add('hidden');
-                hide.classList.add('hidden');
-                showMore.classList.remove('hidden');
-            });
+            slide[i].classList.add('hidden')
         }
     }
 }
+button.addEventListener('click', function () {
+        // Изменяем текст кнопки
+        if (window.innerWidth >= 768 && window.innerWidth < 1067) {
+            if (showMoreText.textContent === 'Скрыть') {
+                showMoreText.textContent = 'Показать все';
+                for (let i = 0; i < slide.length; i++) {
+                    if (i > 5) {
+                        slide[i].classList.add('hidden')
+                    }
+                    showMoreIcon.classList.remove('readMore_rotate')
+                }
+            } else {
+                showMoreText.textContent = 'Скрыть';
+                for (let i = 0; i < slide.length; i++) {
+                    slide[i].classList.remove('hidden')
+                }
+                showMoreIcon.classList.add('readMore_rotate')
+            }
+        }
+        if (window.innerWidth >= 1067) {
+
+            if (showMoreText.textContent === 'Скрыть') {
+                showMoreText.textContent = 'Показать все';
+                for (let i = 0; i < slide.length; i++) {
+                    if (i > 7) {
+                        slide[i].classList.add('hidden')
+                    }
+                    showMoreIcon.classList.remove('readMore_rotate')
+                }
+            } else {
+                showMoreText.textContent = 'Скрыть';
+                for (let i = 0; i < slide.length; i++) {
+                    slide[i].classList.remove('hidden')
+                }
+                showMoreIcon.classList.add('readMore_rotate')
+            }
+        }
+    }
+)
