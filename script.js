a5c275bad4c636298632be91d788beb3501c3d06
Ульяна <panfilovaulyana2016@mail.ru>
2024-10-23 02:23:55 +0500

    финальная версия

--- script.js
+++ script.js
@@ -25,62 +25,120 @@ swiperCard();
 window.addEventListener("resize", swiperCard);
 
 
-let slide = document.querySelectorAll('.brands__item');
-let button = document.querySelector('.readMore');
-let showMoreIcon = document.querySelector('.readMore__icon');
-let showMoreText = document.querySelector('.readMore__text');
+//Раскрытие списка при нажатии на "показать всё"
+let btn = document.querySelectorAll(".readMore");
+let container = document.querySelectorAll(".list");
+let isAgain = false;
+let btnIcon = document.querySelectorAll(".readMore__icon");
+let btnText = document.querySelectorAll(".readMore__text");
 
-
-if (window.innerWidth >= 768 && window.innerWidth < 1067) {
-    for (let i = 0; i < slide.length; i++) {
-        if (i > 5) {
-            slide[i].classList.add('hidden')
-        }
+if (window.innerWidth >= 768) {
+    for (let i = 1; i < btn.length; i++) {
+        btn[i].addEventListener('click', function () {
+                if (!isAgain) {
+                    container[i - 1].style.height = 'auto';
+                    btnIcon[i].classList.add('readMore_rotate');
+                    btnText[i].textContent = 'Скрыть';
+                    isAgain = true;
+                } else {
+                    container[i - 1].removeAttribute("style")
+                    btnIcon[i].classList.remove('readMore_rotate');
+                    btnText[i].textContent = 'Показать все';
+                    isAgain = false;
+                }
+            }
+        )
     }
 }
-if (window.innerWidth >= 1067) {
-    for (let i = 0; i < slide.length; i++) {
-        if (i > 7) {
-            slide[i].classList.add('hidden')
-        }
-    }
-}
-button.addEventListener('click', function () {
-        // Изменяем текст кнопки
-        if (window.innerWidth >= 768 && window.innerWidth < 1067) {
-            if (showMoreText.textContent === 'Скрыть') {
-                showMoreText.textContent = 'Показать все';
-                for (let i = 0; i < slide.length; i++) {
-                    if (i > 5) {
-                        slide[i].classList.add('hidden')
-                    }
-                    showMoreIcon.classList.remove('readMore_rotate')
-                }
-            } else {
-                showMoreText.textContent = 'Скрыть';
-                for (let i = 0; i < slide.length; i++) {
-                    slide[i].classList.remove('hidden')
-                }
-                showMoreIcon.classList.add('readMore_rotate')
-            }
-        }
-        if (window.innerWidth >= 1067) {
 
-            if (showMoreText.textContent === 'Скрыть') {
-                showMoreText.textContent = 'Показать все';
-                for (let i = 0; i < slide.length; i++) {
-                    if (i > 7) {
-                        slide[i].classList.add('hidden')
-                    }
-                    showMoreIcon.classList.remove('readMore_rotate')
-                }
-            } else {
-                showMoreText.textContent = 'Скрыть';
-                for (let i = 0; i < slide.length; i++) {
-                    slide[i].classList.remove('hidden')
-                }
-                showMoreIcon.classList.add('readMore_rotate')
-            }
-        }
+
+//Открыть бургер меню при нажатии на меню
+let btnBurger = document.querySelector('.dropdown')
+let burgerContainer = document.querySelector(".burger__container");
+
+btnBurger.addEventListener('click', function () {
+    burgerContainer.classList.remove('burger__container_hidden');
+})
+
+
+//Закрыть бургер меню на крестик
+let closeBurger = document.querySelector('.close__burger')
+
+closeBurger.addEventListener('click', function () {
+    burgerContainer.classList.add('burger__container_hidden');
+})
+
+
+//Закрыть бургер меню при нажатии на заблюренную зону
+let burgerMenu = document.querySelector('.burger');
+burgerContainer.addEventListener('click', function (e) {
+    let withinBoundaries = e.composedPath().includes(burgerMenu);
+    if (!withinBoundaries) {
+        burgerContainer.classList.add('burger__container_hidden');
     }
-)
+})
+
+//Закрыть фидбэк
+
+let btnClose = document.querySelectorAll('.button_close')
+let feedbackContainer = document.querySelector('.feedback__container');
+
+btnClose[0].addEventListener('click', function () {
+    feedbackContainer.classList.add('feedback__container_hidden');
+})
+
+//Закрыть фидбэк  при нажатии на заблюренную зону
+let feedback = document.querySelector('.feedback');
+feedbackContainer.addEventListener('click', function (e) {
+    let withinBoundaries = e.composedPath().includes(feedback);
+    if (!withinBoundaries) {
+        feedbackContainer.classList.add('feedback__container_hidden');
+    }
+})
+
+//Открыть фидбэк
+let btnChat = document.querySelectorAll('.button__chat')
+
+    btnChat[1].addEventListener('click', function () {
+        feedbackContainer.classList.remove('feedback__container_hidden');
+    })
+
+btnChat[0].addEventListener('click', function () {
+    feedbackContainer.classList.remove('feedback__container_hidden');
+    if (window.innerWidth < 1366) {
+        burgerContainer.classList.add('burger__container_hidden');
+    }
+})
+
+
+//Закрыть call
+
+// let btnClose = document.querySelector('.button_close')
+let call__container = document.querySelector('.call__container');
+
+btnClose[1].addEventListener('click', function () {
+    call__container.classList.add('call__container_hidden');
+})
+
+//Закрыть call при нажатии на заблюренную зону
+let call = document.querySelector('.call');
+call__container.addEventListener('click', function (e) {
+    let withinBoundaries = e.composedPath().includes(call);
+    if (!withinBoundaries) {
+        call__container.classList.add('call__container_hidden');
+    }
+})
+
+//Открыть call
+let btnCall = document.querySelectorAll('.button_call')
+
+btnCall[1].addEventListener('click', function () {
+    call__container.classList.remove('call__container_hidden');
+})
+
+btnCall[0].addEventListener('click', function () {
+    call__container.classList.remove('call__container_hidden');
+    if (window.innerWidth < 1366) {
+        burgerContainer.classList.add('burger__container_hidden');
+    }
+})
